ca98b904a7e74e02dc38214457a7a36aff2b2fc6
Landon <132246737+AllProD1@users.noreply.github.com>
2025-11-20 15:41:04 -0500

    Jav Doc and stubs for Game and Deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,20 +1,34 @@
 import java.util.LinkedList;
 
+/***
+ *Deck
+ * This takes the cards and shuffles them randomly into a LinkedList.
+ * This list then is used when players draw from it thus removing card element.
+ * This also stores and holds the deck here as a linked list.
+ */
 public class Deck {
 
-    //
+    /***
+     * The linked list will hold the card objects of the deck
+     * this functions as the deck of cards for the game it will be shuffled
+     * and it can be drawn from by players
+     * LinkedList was chosen for its speed when shuffling
+     */
     private LinkedList<Card> deck;
 
     /***
-     *
+     *Shuffle method will take the card elements in the linked list
+     * and randomize there current order to deal the cards to the
+     * players at the beginning of the game
+     * it also will take the discard pile and re shuffle it to make
+     * the new deck
      */
-    public void shuffle() {
-        return;
-    }
+    public void shuffle() {}
 
     /***
-     *
-     * @return
+     * The Draw method will return the top card from the shuffled deck
+     * and remove it from the deck linked list and add it to the players hand
+     * @return new Card element from the deck
      */
     public Card Draw() {
         return new Card();
--- src/Game.java
+++ src/Game.java
@@ -1,53 +1,72 @@
 import java.util.ArrayList;
 
+/**
+ * Game
+ * This class allows the game to be played it creates the Hands and Deck
+ * for the game and the players. It also keeps track of whose turn it is
+ * and increments turns played.
+ */
 public class Game {
 
-    //
+    // is an object reference to the linked list for the cards in the drawing or dealing deck
     private Deck gameDeck;
-    //
+    // is an object to reference the linked list for the card objects being discarded
     private Deck discardPile;
 
-    //
+    // int to keep track of which players turn it is
     private int currPlayer;
-    //
+    // int to count the turns of the round and used to increment direction
+    // for reverse card and skip cards
     private int turnIncrement;
 
     /***
-     *
-     * @param numToDraw
-     * @param hand
+     * dealCard will take card elements and enter them into a players hand
+     * depending on the required numToDraw reasoning to an action dealing
+     * or drawing
+     * @param numToDraw is defined by the reason for picking up cards
+     *                  that will be seven at the start of the game
+     *                  two or four depend on the draw card or
+     *                  one card if you cannot play on a turn
+     * @param hand is the arraylist of cards that a player has
      */
     public void dealCard(int numToDraw, ArrayList<Card> hand) {
         return;
     }
 
     /***
-     *
+     * discard is the act of playing by a player removing the card from
+     * their hand and adding it to the discard pile
      */
     public void discard() {
         return;
     }
 
     /***
-     *
+     *The method will allow player x to either play a card out of their
+     * hand or draw a card due to not being able to play a valid card
+     * if the card draw is valid it plays adn advances to next players turn
      */
     public void playTurn() {
         return;
     }
 
     /***
-     *
-     * @param card
-     * @return
+     * isValid checks the current card on the discard pile and returns true or false
+     * if the card played is valid it plays on the discard pile and advances to the
+     * next players turn if the card is not a valid play the player is prompted to
+     * try to play another card from their hand or draw a card from the deck or draw pile
+     * @param card is the object in the players hand that is trying to be played
+     * @return true or false if the card can be played on the discard pile
      */
     public boolean isValid(Card card) {
         return false;
     }
 
     /***
-     *
-     * @param hand
-     * @return
+     * canDrawFour checks to make sure you have no other cards in your hand
+     * that are valid plays if so a player can play the draw four
+     * @param hand is the Arraylist of the players cards
+     * @return true when you have no other valid plays and false when a card plays
      */
     public boolean canDrawFour(ArrayList<Card> hand) {
         return false;
